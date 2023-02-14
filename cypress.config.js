8cf5a5d084a6ec50738199877652055d1f489c4f
Vladimir Tarasov <vtarasov.sk@gmail.com>
2023-02-14 13:24:08 +0100

    First commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 15000,
+  viewportWidth: 1440,
+  viewportHeight: 900,
+  chromeWebSecurity: false,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
